f0e274006f05dbc4951368f0a6dc597dcd300535
Unknown <yanfroes@gmail.com>
2018-10-01 11:08:42 -0300

    Customize until total sales

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,9 +1,13 @@
 class Campaign < ApplicationRecord
   include Fae::BaseModelConcern
 
+  validates :title, presence: true
+  validates :body, presence: true
+
   def fae_display_field
     title
   end
-
+  has_many :campaign_clients
+  has_many :clients, through: :campaign_clients
 
 end
