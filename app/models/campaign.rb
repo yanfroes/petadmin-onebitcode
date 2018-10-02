249f67dd663825c26979f2943ae1a03fba3c7988
Unknown <yanfroes@gmail.com>
2018-10-01 23:17:50 -0300

    Finished

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,13 +1,22 @@
 class Campaign < ApplicationRecord
+
   include Fae::BaseModelConcern
 
   validates :title, presence: true
   validates :body, presence: true
+  has_many :campaign_clients
+  has_many :clients, through: :campaign_clients
+
+  after_save :schedule_emails
 
   def fae_display_field
     title
   end
-  has_many :campaign_clients, dependent: :destroy
-  has_many :clients, through: :campaign_clients
 
+  def schedule_emails
+    Client.find_each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaignJob.perform_later client, self.title, self.body
+    end
+  end
 end
